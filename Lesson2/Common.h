707dd96ec3635aa2a08d93dc517b358ea8a3192c
except134 <except134@gmail.com>
2021-12-09 17:57:43 +0300

    Added WarAndPeace.txt

--- Lesson2/Common.h
+++ Lesson2/Common.h
@@ -14,3 +14,24 @@
 #include <chrono>
 
 #include "Timer.h"
+
+template<typename T>
+inline void SwapPointers(T*& p1, T*& p2)
+{
+    T* t = p1;
+    p1 = p2;
+    p2 = t;
+}
+
+template<typename T>
+inline void SortPointers(std::vector<T*>& v)
+{
+    std::sort(v.begin(), v.end(), [](const T* p1, const T* p2) {return *p1 < *p2; });
+}
+
+template<typename T>
+inline void PrintPointers(const std::vector<T*>& v)
+{
+    std::for_each(v.begin(), v.end(), [](const T* p){ std::cout << *p << " "; });
+    std::cout << std::endl;
+}
--- Lesson2/Lesson.cpp
+++ Lesson2/Lesson.cpp
@@ -1,6 +1,32 @@
 #include "Lesson.h"
 
+const std::vector<char16_t> GLASS_LETTERS{'а','А','е','Е','ё','Ё','и','И','о','О','у','У','э','Э','ы','Ы','ю','Ю','я','Я'};
+
+void CountIfAndFind(const std::string& s)
+{
+
+}
+
 void Lesson()
 {
+    std::cout << "--------- SortPointers ---------" << std::endl;
+    int v1 = 3, v2 = 1, v3 = 20, v4 = 12, v5 = 11;
+    std::vector v{&v1,&v2,&v3,&v4,&v5};
+    PrintPointers(v);
+    SortPointers(v);
+    PrintPointers(v);
+
+    std::cout << "--------- SwapPointers ---------" << std::endl;
+    SwapPointers(v[1], v[3]);
+    PrintPointers(v);
+
+    std::ifstream ifs{"./WarAndPeace.txt"};
+    if(!ifs.is_open())
+        throw "Ошибка открытия файла";
+
+    std::string wap{};
+    std::getline(ifs, wap, '\0');
+
+
 }
 
