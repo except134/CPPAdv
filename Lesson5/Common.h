812028c2fcf8205ec53238dd6110db1be636832a
except134 <except134@gmail.com>
2021-12-10 15:46:46 +0300

    Lesson5 begin

---
+++ Lesson5/Common.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <cstdint>
+#include <tuple>
+#include <map>
+#include <utility>
+#include <fstream>
+#include <string>
+#include <optional>
+#include <sstream>
+#include <regex>
+#include <chrono>
+#include <list>
+#include <memory>
+#include <random>
