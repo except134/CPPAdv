9f7249d8fed236185c84ab5a303f8dba436b6554
except134 <except134@gmail.com>
2021-12-14 12:39:54 +0300

    Lesson5 progress

--- Lesson5/Lesson.cpp
+++ Lesson5/Lesson.cpp
@@ -15,15 +15,31 @@ void PrintUnique(T ib, T ie)
 
 void Task1()
 {
+    std::cout << "---------- Unique ----------" << std::endl;
+
     std::vector<std::string> s{"First", "second", "third", "second"};
     PrintUnique(s.begin(), s.end());
 
     std::vector<int> i{1, 2, 3, 3, 3, 4, 5, 5, 6};
     PrintUnique(i.begin(), i.end());
+
+    std::list<std::string> sl{"First", "second", "third", "second"};
+    PrintUnique(sl.begin(), sl.end());
+
+    std::list<int> il{1, 2, 3, 3, 3, 4, 5, 5, 6};
+    PrintUnique(il.begin(), il.end());
+}
+
+void Task2()
+{
+    std::cout << "---------- Sentences ----------" << std::endl;
+
+    
 }
 
 void Lesson()
 {
     Task1();
+    Task2();
 }
 
